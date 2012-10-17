cf5da7c48fd5b54fc0b49b3251bcfdfcf8b22d98
Damon Aw <daemonsy@gmail.com>
2012-10-17 06:40:59 +0100

    Fixes

--- app/models/store_credit_minimum_validator.rb
+++ app/models/store_credit_minimum_validator.rb
@@ -1,10 +1,11 @@
 class StoreCreditMinimumValidator < ActiveModel::Validator
   def validate(record)
     if Spree::Config[:use_store_credit_minimum] and record.item_total < Spree::Config[:use_store_credit_minimum].to_f and record.store_credit_amount > 0
+      if record.user.store_credits_total < Spree::Config[:use_store_credit_minimum].to_f
+        record.errors[:base] << "You must have $#{ Spree::Config[:use_store_credit_minimum].to_s } to redeem."
+      end
       record.errors[:base] <<  "Order's item total is less than the minimum allowed (#{Spree::Config[:use_store_credit_minimum].to_f}) to use store credit."
     end
-     if record.user.store_credits_total < Spree::Config[:use_store_credit_minimum].to_f
-      record.errors[:base] << "You must have #{number_to_currency Spree::Config[:use_store_credit_minimum]} to redeem."
-    end
+
   end
 end
